73957a8525cd26f18616b8a7eb964199daa86729
portiaportia <portiaplante@gmail.com>
2023-10-10 13:51:12 -0400

    fixed #15

--- Recipe.java
+++ Recipe.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 public class Recipe {
     private String recipeName;
     private ArrayList<String> ingreedients;
+    private ArrayList<String> directions;
 
     public Recipe(String recipeName){
         this.recipeName = recipeName;
