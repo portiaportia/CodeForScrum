426552e94bc2f99fd71b874c7677607f88114e9c
portiaportia <portiaplante@gmail.com>
2023-10-10 13:50:28 -0400

    added ArrayList to recipe #15

--- Recipe.java
+++ Recipe.java
@@ -1,5 +1,8 @@
+import java.util.ArrayList;
+
 public class Recipe {
     private String recipeName;
+    private ArrayList<String> ingreedients;
 
     public Recipe(String recipeName){
         this.recipeName = recipeName;
