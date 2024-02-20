b11b24d9af0a3a106bcbd384271ec5decb5af61a
portiaportia <portiaplante@gmail.com>
2024-02-20 14:05:06 -0500

    fixed #12

--- DataLoader.java
+++ DataLoader.java
@@ -3,4 +3,8 @@ public class DataLoader {
     public static ArrayList<User> getUsers(){
         return new ArrayList<User>();
     }
+
+    public static ArrayList<Recipe> getRecipes() {
+        return new ArrayList<Recipe>();
+    }
 }
